d8717c2475cb3262b9e780e5dcb7b325959d9c75
Charan DVVK <charandvvk911@gmail.com>
2024-06-19 23:24:04 +0530

    attempt to fix railway error

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const { PORT } = process.env;
 
-app.listen(PORT, console.log(`Serving on port ${PORT}`));
+app.listen(PORT, "0.0.0.0", console.log(`Serving on port ${PORT}`));
 
 app.use(express.json());
 app.use(cors());
